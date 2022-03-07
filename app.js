91774c8bb30da46bffbdb52b5791ea80af361a4a
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-03-07 13:11:53 -0800

    Completed solo portion of React sprint!

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor ( props ) {
+    super ( props );
+
+    this.state = {
+      done: false,
+      current: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemHover() {
+    this.setState({
+      current: !this.state.current
+    });
+  }
+
+
+  render() {
+    var style = {
+      textDecoration : this.state.done ? 'line-through' : 'none',
+      fontWeight : this.state.current ? 'bold' : 'none'
+    };
+
+    return (
+      <li style = { style } onClick = { this.onListItemClick.bind( this )} onMouseOver = { this.onListItemHover.bind( this )}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+
+var GroceryList = ( props ) => (
+  <ul>
+    {props.groceryItems.map(groceryItem =>
+      <GroceryListItem groceryItem = { groceryItem } />
+    )}
+  </ul>
+)
+
+
+var App = () => (
+  <div>
+    <h2>My GroceryList</h2>
+    <GroceryList groceryItems={ ['Eggs', 'Veggies', 'Milk'] }/>
+  </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// {props.groceryItems.map(( currentItem, index ) => {
+//   return (
+//   <GroceryListItem item = { currentItem }/>
+//   )
+// })}
+
+// var GroceryListItem = ( props ) => (
+//   <li>{ props.item }</li>
+// );
